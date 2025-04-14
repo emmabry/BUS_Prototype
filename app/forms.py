44bc17e9a1961892784b09b637b291c9f6c78316
emma <emmaabryan@gmail.com>
2025-04-14 13:56:37 +0100

    add signup & login functionality, add landing page & skeleton for dashboard & quiz

--- app/forms.py
+++ app/forms.py
@@ -13,7 +13,16 @@ class ChooseForm(FlaskForm):
     choice = HiddenField('Choice')
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    university = StringField('University', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
